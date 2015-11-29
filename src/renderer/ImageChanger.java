a37c8a957ccb42ebc7d7a873e08177641a0aa7e3
simar1998 <simarpal.kalsi@gmail.com>
2015-11-29 14:00:12 -0500

    added methods to the image changer class wchihch change the image of the load and new game method.

--- src/renderer/ImageChanger.java
+++ src/renderer/ImageChanger.java
@@ -9,16 +9,44 @@ public class ImageChanger {
 
 	public static Image mainMenuContinue(String s, JButton button){
 		if(s.equals("click")){
-			Image continueClick = new ImageIcon("Assest/Pictures/MainScreen/continue_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			return continueClick;
+			Image Click = new ImageIcon("Assest/Pictures/MainScreen/continue_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Click;
 		}
 		if(s.equals("hover")){
-			Image continueHover = new ImageIcon("Assest/Pictures/MainScreen/continue_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			return continueHover;
+			Image Hover = new ImageIcon("Assest/Pictures/MainScreen/continue_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Hover;
 		}
 		else {
 			Image continueIdle = new ImageIcon("Assest/Pictures/MainScreen/continue_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return continueIdle;
+		}
+	}
+	public static Image mainMenuLoad(String s, JButton button){
+		if(s.equals("click")){
+			Image Click = new ImageIcon("Assest/Pictures/MainScreen/load_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Click;
+		}
+		if(s.equals("hover")){
+			Image Hover = new ImageIcon("Assest/Pictures/MainScreen/load_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Hover;
+		}
+		else {
+			Image Idle = new ImageIcon("Assest/Pictures/MainScreen/load_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Idle;
+		}
+	}
+	public static Image mainMenuNewGame(String s, JButton button){
+		if(s.equals("click")){
+			Image Click = new ImageIcon("Assest/Pictures/MainScreen/newGame_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Click;
+		}
+		if(s.equals("hover")){
+			Image Hover = new ImageIcon("Assest/Pictures/MainScreen/newGame_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Hover;
+		}
+		else {
+			Image Idle = new ImageIcon("Assest/Pictures/MainScreen/newGame_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			return Idle;
 		}
 	}
 }
