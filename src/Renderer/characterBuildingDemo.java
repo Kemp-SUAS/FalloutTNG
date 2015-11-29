ef456539b0ee07a88f59b629cc72e73a4f2e63c3
simar1998 <simarpal.kalsi@gmail.com>
2015-11-29 11:15:02 -0500

    Made the image load dynamic so all platforms can load the image and apply to the button

--- src/Renderer/characterBuildingDemo.java
+++ src/Renderer/characterBuildingDemo.java
@@ -7,12 +7,16 @@ import java.awt.Image;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class characterBuildingDemo extends JFrame {
 
@@ -36,8 +40,9 @@ public class characterBuildingDemo extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @throws IOException 
 	 */
-	public characterBuildingDemo() {
+	public characterBuildingDemo() throws IOException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1048, 600);
 		contentPane = new JPanel();
@@ -48,13 +53,13 @@ public class characterBuildingDemo extends JFrame {
 		JButton continueButton = new JButton("");
 		continueButton.setBounds(840, 291, 182, 47);
 		contentPane.add(continueButton);
-		Image continueIdle = new ImageIcon("/FalloutTNG/Assest/Pictures/MainScreen/continue_Idle.png").getImage().getScaledInstance(continueButton.getWidth(),continueButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
+		Image continueIdle = new ImageIcon("Assest/Pictures/MainScreen/continue_Idle.png").getImage().getScaledInstance(continueButton.getWidth(),continueButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
 		continueButton.setIcon(new ImageIcon(continueIdle));
 		continueButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				Image continueIdle = new ImageIcon("E:\\Downloads\\continue_Idle.png").getImage().getScaledInstance(continueButton.getWidth(),continueButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
-				continueButton.setIcon(new ImageIcon(continueIdle));
+				//Image continueIdle = new ImageIcon("").getImage().getScaledInstance(continueButton.getWidth(),continueButton.getHeight(),java.awt.Image.SCALE_SMOOTH);
+				//continueButton.setIcon(new ImageIcon(continueIdle));
 			}
 		});
 	}
