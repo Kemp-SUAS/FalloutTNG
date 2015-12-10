68a01bc83f3d75659dab0987603bc43636a5e0e0
KieranWilson10 <kwhockey10@gmail.com>
2015-12-09 19:16:39 -0500

    Updated Constants

--- src/AssetHandling/Constants.java
+++ src/AssetHandling/Constants.java
@@ -3,9 +3,10 @@ package AssetHandling;
  * This class will contain all the constants used for the project
  * Please if you want to have constant strings or constant multiplier ints 
  * please do intialize them here
- * @author simar
+ * @author simar , Kieran
  *
  */
 public class Constants {
+	final int CARRY_WEIGHT_CONST = 100;
 
 }
