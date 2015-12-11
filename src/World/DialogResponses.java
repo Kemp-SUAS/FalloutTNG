c3bf073f2c9b38030150a77dadaf3bfc6cee3e2a
Faduma Ahmed <Ahmed.faduma-@hotmail.com>
2015-12-11 13:31:04 -0500

    Dialog Responses with Arrays

--- src/World/DialogResponses.java
+++ src/World/DialogResponses.java
@@ -1,9 +1,21 @@
 package World;
 
 public class DialogResponses {
-
+	
+	
+	
+	
+	private String[] sarcasticOptionLvl1;
+	private String[] normalOptionLvl1;
+	private String[] intelligentOptionLvl1;
+	private String[] dumbOptionLvl1;
+	
 	public DialogResponses() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	
+	
 
 }
