b289ed939ba45e33c05f03c76e03c3851803333b
Simt'pal <simarpal.kalsi@gmail.com>
2015-12-11 21:22:50 -0500

    Revert "Dialog Responses with Arrays"
    This reverts commit c3bf073f2c9b38030150a77dadaf3bfc6cee3e2a.

--- src/World/DialogResponses.java
+++ src/World/DialogResponses.java
@@ -1,21 +1,9 @@
 package World;
 
 public class DialogResponses {
-	
-	
-	
-	
-	private String[] sarcasticOptionLvl1;
-	private String[] normalOptionLvl1;
-	private String[] intelligentOptionLvl1;
-	private String[] dumbOptionLvl1;
-	
+
 	public DialogResponses() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
-	
 
 }
