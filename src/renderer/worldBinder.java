7567ce12c9ba68378286a03aa39f5559890c13a3
MohidAslam <aslammohid@gmail.com>
2015-12-13 16:57:32 -0500

    Added wallStringParser class

--- src/renderer/worldBinder.java
+++ src/renderer/worldBinder.java
@@ -3,6 +3,8 @@
  */
 package renderer;
 
+import java.sql.SQLException;
+
 /**
  * @author simarpal, Faduma Ahmed, Mohid Aslam, Kieran Wilson
  *
@@ -13,5 +15,20 @@ public class worldBinder {
 /**
  * Test world binder
  */
-	
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		 
+		//System.out.println(dataBaseGet("image_strings", 5, "path"));
+		wallStringParser("walls", 1, "wallDimensions");
+		for(int i = 0; int i < wallDimensions, i++){
+			System.out.println(wallDimensions[i]);
+			System.out.println(wallStringParser("walls", 1, "wallDimensions"));
+		}
+	}
+	public static String[] wallStringParser(String tableName, int id, String column) throws SQLException, ClassNotFoundException{
+		String wallInfo = AssetHandling.AssetManager.dataBaseGet(tableName, id, column);
+		String delims = "[/,]+";
+		String[] wallDimensions = wallInfo.split(delims);
+		
+		return wallDimensions;
+	}
 }
