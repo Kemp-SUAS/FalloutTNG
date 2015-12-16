c39bb1bf1c486b7618c03a2ce2e61dcab4a56a33
Faduma Ahmed <Ahmed.faduma-@hotmail.com>
2015-12-16 13:30:20 -0500

    Made a new wall Test Method to draw walls on a JComponent(JFrame)

---
+++ src/renderer/WallTest.java
@@ -0,0 +1,15 @@
+package renderer;
+
+/**
+ * This class uses the Wall.java class to draw lines
+ * @author 322251364
+ *
+ */
+
+public class WallTest {
+
+	public WallTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
