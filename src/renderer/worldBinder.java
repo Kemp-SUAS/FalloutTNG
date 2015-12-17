1413875ab9eb0adb620b3497e9ef7d8806639315
MohidAslam <aslammohid@gmail.com>
2015-12-16 20:30:25 -0500

    Fixed worldBinder wallStringParser method

--- src/renderer/worldBinder.java
+++ src/renderer/worldBinder.java
@@ -6,29 +6,29 @@ package renderer;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import renderer.Wall;
+
 /**
  * @author simarpal, Faduma Ahmed, Mohid Aslam, Kieran Wilson
  *
  */
 public class worldBinder {
 	
-	
 /**
- * This class is an access way from the sqLITE data on to the code.It allows other classes to accces the information
  * Test world binder
  */
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		wallStringParser("1");
-		
+		wallStringParser("1");	
 	}
 	public static void wallStringParser(String level) throws SQLException, ClassNotFoundException{
 		String table = "level_"+level;
 		String wallString = AssetHandling.AssetManager.dataBaseGet(table, 1, "wall");
-		String delims = "[/,]+";
-		String[] wallDimensions = wallString.split(delims);
-		for(int i = 0; i < wallDimensions.length;i++){
-		  System.out.println(wallDimensions[i]);
+		String delims = "[\\/\\,]+";
+		String[] transfer = wallString.split(delims);
+		ArrayList<String> wallDimensions = new ArrayList<String>();
+		for(int i = 0; i < transfer.length;i++){
+			wallDimensions.add(transfer[i]);
 		}
-	
+		wallDimensions.remove(0);
 	}
 }
