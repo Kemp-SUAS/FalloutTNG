0fcca9fa7e4b885231fee967cd07c857593f75ac
fadumaahmed <ahmed.faduma-@hotmail.com>
2015-12-23 08:44:13 -0500

    Added Dialouge Responses and Option classes hopefully we can get a level of dialouge

---
+++ src/world/DialougeResponses.java
@@ -0,0 +1,17 @@
+package world;
+
+/**
+ * 
+ * This class will control what the NPC will say. Essentially the NPC’s will
+ * assist the user in the game by giving them clues or request that they
+ * complete the quest. The NPC will only say specific sentences based on the
+ * users progress in the game(how many quests they have achieved and their stats
+ * like health, toxin level etc.). There are a series of things the NPC could
+ * say . This class breaks down those options and output certain information.
+ * 
+ * @author Faduma Ahmed Mohid Aslam Kieran Wilson Simar Pal Kalsi
+ *
+ */
+public class DialougeResponses {
+
+}
