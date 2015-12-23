8ca67efa892ffeb29c126fbf2a08cff4af288417
fadumaahmed <ahmed.faduma-@hotmail.com>
2015-12-23 08:41:34 -0500

    Added a new Class Interface.java

---
+++ src/world/Interface.java
@@ -0,0 +1,14 @@
+package world;
+
+
+/**
+ *  This class helps with enabling the user to communicate to the program.
+ *   It will deal with the visual aspect of what the game will look like as well as directions on the users input.
+ *    It will interact with the inventory of raw files( from Package Renderer) and level.java.
+
+ * @author Faduma Ahmed Mohid Aslam Kieran Wilson Simar Pal Kalsi
+ *
+ */
+public class Interface {
+
+}
