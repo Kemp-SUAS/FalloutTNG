2bf9fc2bbfb9827e5105f93237b8626529db1bf6
fadumaahmed <ahmed.faduma-@hotmail.com>
2015-12-23 08:10:05 -0500

    Minor changes To CharacterBinding.java

--- src/vault1/CharacterFolder/CharacterBinding.java
+++ src/vault1/CharacterFolder/CharacterBinding.java
@@ -1,5 +1,12 @@
 package vault1.CharacterFolder;
 
+/**
+ * This class will comply the player data from the class load.java and set the
+ * world as well as the player stats and the progress of the game.
+ *
+ * @author Faduma Ahmed Mohid Aslam, Kieran Wilson Simar Pal Kalsi
+ *
+ */
 public class CharacterBinding {
-	
+
 }
