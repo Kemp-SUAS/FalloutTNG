2bd934903b4af8caabbc1bd90e8f3e2d0f8c0e89
fadumaahmed <ahmed.faduma-@hotmail.com>
2015-12-23 08:39:31 -0500

    Minor Changes

--- src/world/WorldLayout.java
+++ src/world/WorldLayout.java
@@ -1,5 +1,11 @@
 package world;
 
+/**
+ * This class helps with the layout of the program.It controls the world map and the Npc. 
+ * Npc are non-playable characters that interact with the user via dialogue. This class will bind with NPC as well as Player Movements.
+ * @author Faduma Ahmed Mohid Aslam Kieran Wilson Simar pal Kalsi
+ *
+ */
 public class WorldLayout {
 
 	public WorldLayout() {
