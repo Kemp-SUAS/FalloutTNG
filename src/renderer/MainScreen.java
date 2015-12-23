78b78671f01bcc524ff154eb3538ee5ec8a971b8
fadumaahmed <ahmed.faduma-@hotmail.com>
2015-12-23 04:16:26 -0500

    Changes to MainScreen.java

--- src/renderer/MainScreen.java
+++ src/renderer/MainScreen.java
@@ -29,34 +29,49 @@ import AssetHandling.AssetManager;
  * 
  * @author Faduma Ahmed, Mohid Aslam, Kieran Wilson Simarpal Kalsi
  * 
- * This class shows the main screen to the user. This will be the the initial interaction the player will have with the game
+ * This class shows the main screen to the user. This will be the the initial interaction the player will have with the game. This class sets the frame an all mouse related actions
  *
  */
 
 public class MainScreen extends JFrame {
 
+	// create a generic lightweight container.
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Launch the application.It sets the screen and the frame size settings
+	 * 
+	 * 
 	 */
 	public static void main(String[] args) {
+		//that queues events, an implements the runnable interface
 		EventQueue.invokeLater(new Runnable() {
+			
+			/**
+			 * inherited method from Runnable interface
+			 * @see java.lang.Runnable#run()
+			 */
 			public void run() {
+				
 				try {
-					MainScreen frame = new MainScreen();
-					frame.setVisible(true);
-	
-					frame.setResizable(false);
-				} catch (Exception e) {
-					e.printStackTrace();
+					// creates a new object from this class
+					MainScreen  frame =  new MainScreen ();
+					// sets the visibility of the frame
+					frame . setVisible  (true );
+					// Prohibts the user from changing the size of the frame
+					frame . setResizable (false );
+				} 
+				// indicates conditions that a reasonable application might want to catch.
+				catch ( Exception  e ) {
+					e. printStackTrace( );
 				}
 			}
 		});
-	}
+	} 
 
 	/**
-	 * Create the frame.
+	 * THis constructors creates the frame and sets all required settings
+	 * 
 	 * 
 	 * @throws IOException
 	 */
@@ -69,18 +84,27 @@ public class MainScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		*/
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1048, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		// Sets the operation that will happen by default when the user initiates a "close" on this frame
+		setDefaultCloseOperation( JFrame . EXIT_ON_CLOSE );
+		// Moves and resizes this component.
+		setBounds ( 100 , 100 , 1048 , 600);
+		// Creates a new JPanel with a double buffer and a flow layout.
+		contentPane  =  new  JPanel( );
+		// Sets the borders, object is responsible for defining the insets for the component 
+		contentPane. setBorder ( new  EmptyBorder (5, 5, 5, 5));
+		
 		setContentPane(contentPane);
+		// Sets the layout manager for the container.
 		contentPane.setLayout(null);
+		// Creates a new button
 		JButton continueButton = new JButton("");
+		// Moves and resizes this the button.
 		continueButton.setBounds(840, 191, 182, 47);
+		// Appends the specified component to contentPane
 		contentPane.add(continueButton);
 
 		try {
+			//
 			continueButton.setIcon(new ImageIcon(imageMaker("image_strings", "path", 3, continueButton)));
 		} catch (ClassNotFoundException e1) {
 			e1.printStackTrace();
@@ -99,6 +123,10 @@ public class MainScreen extends JFrame {
 					e1.printStackTrace();
 				}
 			}
+			
+			
+			
+			
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
