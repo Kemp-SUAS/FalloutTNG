1ced4076b3b6b74b55cccd60ba47565fa301bb16
fadumaahmed <ahmed.faduma-@hotmail.com>
2016-01-05 13:37:28 -0500

    Top Down Scrolling Not Done

---
+++ src/renderer/TopDownlaunch.java
@@ -0,0 +1,14 @@
+package renderer;
+
+public class TopDownlaunch {
+
+	public static void main(String[] args) {
+		
+		
+		TopDownScroller TopDwn= new TopDownScroller();
+		
+		TopDwn.start();
+
+	}
+
+}
