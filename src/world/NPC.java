686cd689bff576e995c9c63ebd2743edb3981ccd
Simt'pal <simarpal.kalsi@gmail.com>
2016-01-06 13:11:29 -0500

    Revert "Minor Comment to NPC.java"
    This reverts commit 9540d8f10cd55ec9552829a597fc1acf8b3bac0e.

--- src/world/NPC.java
+++ src/world/NPC.java
@@ -1,13 +1,5 @@
 package world;
 
-/**
- * This class controls the NPC 
- * information: how they will look where and when they appear as well 
- * as their interaction with the player. Level. java will implement this class to load information and player progress. 
-
- * @author Faduma Ahmed Mohid Aslam Kieran Wilson Simar Pal kalsi
- *
- */
 public class NPC {
 
 	public NPC() {
