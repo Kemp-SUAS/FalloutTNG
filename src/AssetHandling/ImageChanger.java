236e1dbf65fa33ee5f7f1675ae1c5e756ec83654
Simt'pal <simarpal.kalsi@gmail.com>
2016-01-06 13:11:48 -0500

    Revert "Comments/Javadoc"
    This reverts commit 00d8fecda5f4b712b44ef57a2bfbc99766813834.

--- src/AssetHandling/ImageChanger.java
+++ src/AssetHandling/ImageChanger.java
@@ -43,31 +43,21 @@ public class ImageChanger {
 	 * @param button, the button from the JFrame
 	 * @return Image, the image when the user clicks the button
 	 */
-	public static Image   mainMenuLoad(String s, JButton button){
+	public static Image mainMenuLoad(String s, JButton button){
 		
 		// If the user clicks the button
 		if(s.equals("click")){
-			// Creates an ImageIcon if the user clicks the buttton. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			Image Click =   new ImageIcon("Assets/Pictures/MainScreen/load_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			// Creates an ImageIcon from the specified file. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
+			Image Click = new ImageIcon("Assets/Pictures/MainScreen/load_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			// returns the image 
 			return Click;
 		}
-		
-		
 		if(s.equals("hover")){
-			// Creates an ImageIcon if the user hovers over the button. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			
-			
-			Image Hover =   new ImageIcon("Assets/Pictures/MainScreen/load_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			
-			// returns the image
+			Image Hover = new ImageIcon("Assets/Pictures/MainScreen/load_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Hover;
 		}
 		else {
-			// Creates an ImageIcon if the user is idle over the button. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			
-			Image Idle =  new ImageIcon("Assets/Pictures/MainScreen/load_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			// returns the image
+			Image Idle = new ImageIcon("Assets/Pictures/MainScreen/load_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Idle;
 		}
 	}
@@ -78,32 +68,17 @@ public class ImageChanger {
 	 * @return Image
 	 */
 	public static Image mainMenuNewGame(String s, JButton button){
-		
-		// If the user clicks the button
 		if(s.equals("click")){
-			
-			// Creates an ImageIcon if the user clicks the buttton. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			
-			Image Click =   new  ImageIcon("Assets/Pictures/MainScreen/newGame_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			// returns the image 
+			Image Click = new ImageIcon("Assets/Pictures/MainScreen/newGame_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Click;
 		}
-		// If the user hovers over the button
 		if(s.equals("hover")){
-			// Creates an ImageIcon if the user hovers over the button. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			
-			Image Hover =  new  ImageIcon("Assets/Pictures/MainScreen/newGame_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			Image Hover = new ImageIcon("Assets/Pictures/MainScreen/newGame_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Hover; 
 			
 		}
-		// If the user is idle around the button
 		else {
-			
-			// Creates an ImageIcon if the user is idle around the button. The image will be preloaded by using MediaTracker to monitor the loading state of the image. 
-			
-			Image  Idle = new ImageIcon("Assets/Pictures/MainScreen/newGame_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
-			// returns the image 
-			
+			Image Idle = new ImageIcon("Assets/Pictures/MainScreen/newGame_Idle.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Idle;
 		}
 	}
@@ -115,11 +90,11 @@ public class ImageChanger {
 	 */
 	public static Image mainMenuExitGame(String s, JButton button){
 		if(s.equals("click")){
-			Image  Click = new  ImageIcon("Assets/Pictures/MainScreen/exit_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			Image Click = new ImageIcon("Assets/Pictures/MainScreen/exit_click.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Click;
 		}
 		if(s.equals("hover")){
-			Image Hover = new  ImageIcon("Assets/Pictures/MainScreen/exit_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
+			Image Hover = new ImageIcon("Assets/Pictures/MainScreen/exit_hover.png").getImage().getScaledInstance(button.getWidth(),button.getHeight(),java.awt.Image.SCALE_SMOOTH);
 			return Hover;
 		}
 		else {
